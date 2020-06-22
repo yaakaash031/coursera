4adf60766fbfeba9e1083fedd7edf8c902d3964b
yaakaash031 <67217639+yaakaash031@users.noreply.github.com>
2020-06-22 20:29:35 +0530

    Add files via upload

---
+++ module4/js/script.js
@@ -0,0 +1,10 @@
+
+var arr=["joky","Aman","Jhon","Aakash","Kapil","sonia","joky","rahul"];
+for(var i=0;i<arr.length;i++){
+
+	 if(arr[i][0]=="j" || arr[i][0]=="J")
+			console.log("Goodbye " + arr[i]);
+
+	 else console.log("hello " + arr[i]); 
+}	
+
